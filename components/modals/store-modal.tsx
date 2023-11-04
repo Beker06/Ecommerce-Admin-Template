e3107a01f2bc4f5efa1989caf083801343a9c1ac
Eliam Vazquez <bosc790@gmail.com>
2023-11-04 14:42:21 -0700

    V0.2.0

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -36,8 +36,7 @@ export const StoreModal = () => {
         try {
           setLoading(true);
           const response = await axios.post('/api/stores', values);
-          toast.success("Store created.")
-          //window.location.assign(`/${response.data.id}`);
+          window.location.assign(`/${response.data.id}`);
         } catch (error) {
           console.log(error);
           toast.error('Something went wrong');
