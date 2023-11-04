8ecd64b64d277bf2b42e5a91c75eb8f3e3d47249
Eliam Vazquez <bosc790@gmail.com>
2023-11-04 14:48:53 -0700

    V0.2.1

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,8 +1,25 @@
+import prismadb from "@/lib/prismadb";
 
+interface DashboardPageProps {
+    params: {
+      storeId: string;
+    };
+  };
 
-const DashboardPage = () => {
+  const DashboardPage: React.FC<DashboardPageProps> = async ({ 
+    params
+  }) => {
+
+    const store = await prismadb.store.findFirst({
+        where: { 
+            id: params.storeId
+        }
+    })
+
   return (
-    <div>This is a dashboard</div>
+    <div>
+        Active Store: {store?.name}
+    </div>
   )
 }
 
