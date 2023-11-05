f96f08219763382f8a85f2feafb8b5527761bd18
Eliam Vazquez <bosc790@gmail.com>
2023-11-04 17:50:15 -0700

    V0.3.2

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,8 +1,37 @@
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
 
-const SettingsPage = () => {
-  return (
-    <div>Settings Page</div>
-  )
+import prismadb from "@/lib/prismadb";
+
+const SettingsPage = async ({
+    params
+}:{
+    params: { storeId: string }
+}) => {
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect('/sign-in');
+    }
+
+    const store = await prismadb.store.findFirst({
+        where: {
+        id: params.storeId,
+        userId
+        }
+    });
+
+    if (!store) {
+        redirect('/');
+    }
+
+    return (
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                {/* <SettingsForm initialData={store} /> */}
+            </div>
+        </div>
+    )
 }
 
 export default SettingsPage
