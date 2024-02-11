9f9e2df5e8db8c7bc12f89b1ffdd19a09c1415fb
Eliam Vazquez <98252478+Beker06@users.noreply.github.com>
2024-02-10 22:49:46 -0800

    [V0.10.0]Products Entity Admin

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -65,7 +65,7 @@ export function MainNav({
           key={route.href}
           href={route.href}
           className={cn(
-            'text-sm font-medium transition-colors hover:text-primary',
+            'text-sm font-medium transition-colors hover:text-primary dark:hover:text-white',
             route.active ? 'text-black dark:text-white' : 'text-muted-foreground'
           )}
         >
