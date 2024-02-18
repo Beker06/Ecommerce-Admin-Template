d64a63c94a5690c92bc94bc46eb69c678ba739ca
Eliam Vazquez <98252478+Beker06@users.noreply.github.com>
2024-02-17 22:40:58 -0800

    [V0.13.1] Loader + Darkmode

--- app/(dashboard)/[storeId]/(routes)/settings/loading.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/loading.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-// import { Loader } from "@/components/ui/loader";
+import { Loader } from "@/components/ui/loader";
 
 const Loading = () => {
   return ( 
     <div className="flex h-full w-full items-center justify-center">
-      {/* <Loader /> */}
+      <Loader />
     </div>
-   );
+  );
 }
- 
+
 export default Loading;
